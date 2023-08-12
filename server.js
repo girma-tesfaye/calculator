a4c7fa7a0e5193f200e0ef851f1bd24f6b72be29
girma-tesfaye <girmatesfaye80@gmail.com>
2023-08-12 02:49:47 -0700

    Layout updated

--- server.js
+++ server.js
@@ -1,18 +1,45 @@
 const http = require('http');
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
 
-const handleFunction = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile("./public/index.html", null, (error, data)=>{
-        if (error) {
-            response.writeHead(404);
-            response.write('Whoops! File not found!');
+const server = http.createServer((req, res) => {
+    // Assuming your HTML and CSS files are in the same directory as server.js
+    const filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    const extname = path.extname(filePath);
+    const contentType = getContentType(extname);
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // File not found
+                res.writeHead(404);
+                res.end('File not found');
+            } else {
+                // Server error
+                res.writeHead(500);
+                res.end('Server error');
+            }
         } else {
-            response.write(data);
+            // Successful response
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
         }
-        response.end();
-})
+    });
+});
+
+// Start the server on port 3000
+server.listen(8000, () => {
+    console.log('Server is running on http://localhost:8000');
+});
+
+// Function to determine content type based on file extension
+function getContentType(extname) {
+    switch (extname) {
+        case '.html':
+            return 'text/html';
+        case '.css':
+            return 'text/css';
+        default:
+            return 'application/octet-stream';
+    }
 }
-http.createServer(handleFunction).listen(8000, ()=> console.log("Hey man"));
